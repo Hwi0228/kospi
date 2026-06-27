28dafe690c1a9de868e0c1d9c005e30253e99d6b
otter7214 <your-email@example.com>
2026-06-27 11:38:47 +0900

    dfadsfasdf

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import pandas as pd
 
-kospi = yf.Ticker('^KS11')
+kospi = yf.Ticker('GC=F')
 data = kospi.history(start="2025-06-27",end="2026-06-27")
 data = data.reset_index()
 
@@ -13,9 +13,9 @@ price = [[x] for x in price]
 
 time_day = data['Date'].dt.dayofyear.values.tolist()
 time_year = data['Date'].dt.year.values.tolist()
-time = [[sum(x)] for x in zip(time_day+time_year)]
+time = [[x[0]+365*x[1]] for x in zip(time_day,time_year)]
 
-poly_feature = preprocessing.PolynomialFeatures(degree=2, include_bias=False)
+poly_feature = preprocessing.PolynomialFeatures(degree=5, include_bias=False)
 time_poly = poly_feature.fit_transform(time)
 print('A_poly =', time_poly)
 
@@ -30,10 +30,15 @@ colors = cmap = plt.get_cmap('seismic')
 #===========================================#
 
 plt.scatter(time, price)
+
 predict_time = time.copy()
-predict_time.extend([[x[0]+31] for x in time])
-print(predict_time)
+
+last_day = time[-1][0]
+future_time = [[last_day + i] for i in range(1, 365)]
+predict_time.extend(future_time)
+
 predict_time_poly = poly_feature.fit_transform(predict_time)
 y_pred = regr.predict(predict_time_poly)
+
 plt.plot(predict_time, y_pred, 'r')
 plt.show()
